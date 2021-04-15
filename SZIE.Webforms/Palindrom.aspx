217220d04f26a6b98b8b5004efdb0e82bc59578d
jamesonatic <jamesonatic@users.noreply.github.com>
2021-04-15 12:04:45 +0200

    Palindrom checker
    added new feature palindrom checker,
    compiler bugfix

---
+++ SZIE.Webforms/Palindrom.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Palindrom.aspx.cs" Inherits="WebApplication4.Palidrom" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h1>Palindrom kereső</h1>
+    <p>
+        Kérem adjon meg egy mondatot: <br />
+        <asp:TextBox ID="txtSentence" runat="server" Width="346px"></asp:TextBox>
+        <asp:Button ID="btnResult" runat="server" Text="Kiértékel" OnClick="btnResult_Click" />
+        <asp:Label ID="lblResult" runat="server" Text="Label"></asp:Label>
+    </p>
+</asp:Content>
---
+++ SZIE.Webforms/Palindrom.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+{
+    public partial class Palidrom : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnResult_Click(object sender, EventArgs e)
+        {
+            var isPalindrom = true;
+            var text = txtSentence.Text.Replace(" ", "").ToLower();
+            for (int i = 0; i<text.Length;i++)
+            {
+                if (text[i] != text[text.Length-1-i])
+                {
+                    isPalindrom = false;
+                    break;
+                }
+            }
+            if (isPalindrom)
+            {
+                lblResult.Text = "Igen, ez palindrom!";
+                lblResult.BackColor = System.Drawing.Color.LightGreen;
+            }
+            else
+            {
+                lblResult.Text = "Nem, ez nem palindrom!";
+                lblResult.BackColor = System.Drawing.Color.OrangeRed;
+            }
+        }
+    }
+}
