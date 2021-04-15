570a225a2c12e06e0d9afdd4ae56268ba4baab5d
jamesonatic <jamesonatic@users.noreply.github.com>
2021-04-15 14:42:43 +0200

    added shopping basics
    configured DB connection
    added new shopping form

---
+++ SZIE.Webforms/Shopping/AddShoppingOccation.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AddShoppingOccation.aspx.cs" Inherits="WebApplication4.Shopping.AddShoppingOccation" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <p>
+        <asp:Calendar ID="cndDate" runat="server"></asp:Calendar>
+        <asp:TextBox ID="txtDescription" runat="server" Width="319px"></asp:TextBox>
+        <asp:Button ID="btnSubmit" runat="server" Text="Add" OnClick="btnSubmit_Click" />
+        <asp:Literal ID="ltrResult" runat="server"></asp:Literal>
+
+
+    </p>
+</asp:Content>
---
+++ SZIE.Webforms/Shopping/AddShoppingOccation.aspx.cs
@@ -0,0 +1,42 @@
+﻿using ShoppingDataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4.Shopping
+{
+    public partial class AddShoppingOccation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            var selectedDate = cndDate.SelectedDate;
+            var descr = txtDescription.Text;
+
+            using (var db = new ShoppingContext())
+            {
+                var isExist = db.ShoppingOccasions.Any(x => x.Date.Equals(selectedDate));
+                if (isExist)
+                    ltrResult.Text = "This date is already exists.";
+                else
+                {
+                    var query = db.ShoppingOccasions.Add(new ShoppingOccasion()
+                    {
+                        Date = cndDate.SelectedDate,
+                        Description = descr
+                    }) ;
+                    db.SaveChanges();
+                    ltrResult.Text = selectedDate.ToShortDateString() + ": " + txtDescription.Text;
+                }
+            }
+
+        }
+    }
+}
