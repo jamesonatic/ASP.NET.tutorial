9c9f320b723997aa6b2df460a9d0cc4cc3bdfc54
jamesonatic <jamesonatic@users.noreply.github.com>
2021-04-15 14:37:05 +0200

    Added database layer
    added classes to store the shoppings
    added Entity management

---
+++ ShoppingDataAccess/Models/ShoppingContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingDataAccessLayer.Models
+{
+    public class ShoppingContext : DbContext
+    {
+        public ShoppingContext() : base("name=MyConnString")
+        {
+            Database.SetInitializer<ShoppingContext>(new CreateDatabaseIfNotExists<ShoppingContext>());
+        }
+        public virtual DbSet<ShoppingOccasion> ShoppingOccasions { get; set; }
+        public virtual DbSet<ShoppingItem>  ShoppingItems { get; set; }
+        public virtual DbSet<ShoppingPlace> ShoppingPlaces  { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ShoppingOccasion>().ToTable("ShoppingOccation");
+            modelBuilder.Entity<ShoppingOccasion>().HasKey(x => x.ShoppinOccasionID);
+            modelBuilder.Entity<ShoppingOccasion>().Property(x => x.Description).HasMaxLength(50);
+
+            modelBuilder.Entity<ShoppingPlace>().ToTable("ShoppinPlace");
+            modelBuilder.Entity<ShoppingPlace>().HasKey(x => x.ShoppingPlaceID);
+            modelBuilder.Entity<ShoppingPlace>().Property(x => x.Address).HasMaxLength(200);
+            modelBuilder.Entity<ShoppingPlace>().Property(x => x.Name).HasMaxLength(50);
+
+            modelBuilder.Entity<ShoppingItem>().ToTable("ShoppingItem");
+            modelBuilder.Entity<ShoppingItem>().HasKey(x => x.ShoppingItemID);
+            modelBuilder.Entity<ShoppingItem>().Property(x => x.ItemName).HasMaxLength(50);
+            modelBuilder.Entity<ShoppingItem>().Property(x => x.Unit).HasMaxLength(10);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ShoppingDataAccess/Models/ShoppingItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingDataAccessLayer.Models
+{
+    public class ShoppingItem
+    {
+        public long ShoppingItemID { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public string Unit { get; set; }
+        public virtual ShoppingOccasion Occasion { get; set; }
+        public virtual ShoppingPlace Place { get; set; }
+    }
+}
